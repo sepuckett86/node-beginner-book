2a7ef85d09082b7c70ed7d08cfaf263a4e56985d
sepuckett86 <sepuckett86@gmail.com>
2019-06-19 16:48:34 -0700

    pass response as parameter

--- server.js
+++ server.js
@@ -5,15 +5,10 @@ function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received.');
-        route(handle, pathname);
-        var content = route(handle, pathname);
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write(content);
-        response.end();
+        route(handle, pathname, response);
     }
     http.createServer(onRequest).listen(8888);
 }
-
 
 console.log('Server has started');
 
