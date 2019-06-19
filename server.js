4e5be7580fdf9543837cab2e4a504cd6bb3cce73
sepuckett86 <sepuckett86@gmail.com>
2019-06-19 13:38:48 -0700

    add router.js and wire up

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var http = require("http");
+var http = require('http');
+var url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        console.log("Request received.");
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello world");
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
+        route(pathname);
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello world');
         response.end();
     }
     http.createServer(onRequest).listen(8888);
 }
 
 
-console.log("Server has started");
+console.log('Server has started');
 
 exports.start = start;
