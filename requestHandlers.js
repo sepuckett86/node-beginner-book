39e44b393048b4067eeb33464e94b8ed91eac892
sepuckett86 <sepuckett86@gmail.com>
2019-06-19 16:51:33 -0700

    proof that a long process does not block other path responses

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,11 +4,15 @@ function start(response) {
     console.log('Request handler "start" was called.');
     var content = 'empty';
 
-    exec('ls -lah', function (error, stdout, stderr) {
-        response.writeHead(200, {'Content-Type': 'text/plain'});
+    exec("find /", { 
+        timeout: 10000, 
+        maxBuffer: 20000*1024 
+    },
+    function (error, stdout, stderr) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
         response.write(stdout);
         response.end();
-    });   
+    });
 }
 
 function upload(response) {
