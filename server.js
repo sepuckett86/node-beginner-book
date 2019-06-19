60e1575197489a3427f10ab10285d776d78bf0fd
sepuckett86 <sepuckett86@gmail.com>
2019-06-19 16:24:54 -0700

    do changes that are not recommended to see why

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received.');
         route(handle, pathname);
+        var content = route(handle, pathname);
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write('Hello world');
+        response.write(content);
         response.end();
     }
     http.createServer(onRequest).listen(8888);
