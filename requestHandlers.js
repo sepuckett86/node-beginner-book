34e1121d0813461df2f6f1d88ca6a98e9b66ac4d
sepuckett86 <sepuckett86@gmail.com>
2019-06-19 17:19:25 -0700

    got png to load at /show

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,5 @@
-var querystring = require('querystring');
+var querystring = require('querystring')
+var fs = require('fs');
 
 function start(response, postData) {
     console.log('Request handler "start" was called.');
@@ -27,5 +28,13 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response) {
+    console.log('Request handler "show" was called.');
+    response.writeHead(200, {'Content-Type': 'image/png'});
+
+    fs.createReadStream('tmp/test.png').pipe(response);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
